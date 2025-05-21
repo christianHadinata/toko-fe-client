c48e748249bc739f2d8a6c55d56c48917a647a03
Shawn <myking123321@gmail.com>
2025-05-21 19:25:36 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Router, Route } from "@solidjs/router";
 
 import Login from "./routes/login";
 import page404 from "./routes/404";
+import Register from "./routes/register";
 
 function App() {
   return (
     <>
    
       <Router>
+          <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="*" component={page404}></Route>
       </Router>
---
+++ src/routes/register.jsx
@@ -0,0 +1,82 @@
+import { A } from '@solidjs/router'
+import whiteLogo from "../assets/white.png"
+import { createEffect, createSignal } from 'solid-js'
+
+
+function Register(){
+    const [isVisible, setIsVisible] = createSignal(false);
+    const toggleVisibility = ()=> setIsVisible(!isVisible);
+
+    return (
+        <>
+        <div class='flex'>
+            <div class="flex h-screen w-1/2 items-center justify-center rounded-br-full bg-sky-400">
+                <div class="mb-32 ml-32 flex items-center justify-center">
+                    <img src={whiteLogo} width={150} height={150} alt="logo" />
+                    <h1 class="cursor-default text-6xl text-white">E-Commerce</h1>
+                </div>
+            </div>
+            <div class='flex h-screen w-1/2 flex-col items-center justify-center'>
+                <div class='w-1/2'>
+                    <A
+                    href={'/'}
+                    class="text-xl font-normal text-sky-400 hover:text-sky-300"
+                    >
+                    {'< '}Back to home
+                    </A>
+                    <div class="mt-2 p-10 shadow-lg shadow slate-200">
+                        <h1 class='pb-10 text-center text-4xl font-semibold'>Login</h1>
+                        <form class="flex flex-col gap-y-5" onSubmit={''}>
+                            <label class="mb-1 text-gray-700 font-medium"> Username</label>
+                            <input 
+                            type="text"
+                            value=""
+                            onChange={''}
+                            class='px-4 py-3 rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 text-base'
+                            
+                            />
+                            <label class="mb-1 text-gray-700 font-medium"> Email</label>
+                            <input 
+                            type="email"
+                            value=""
+                            onChange={''}
+                            class='px-4 py-3 rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 text-base'
+                            />
+                            <label class="mb-1 text-gray-700 font-medium"> Password</label>
+                            <div class="relative w-full max-w-md">
+                                <input 
+                                type={isVisible() ? "text" : "password"}
+                                value=""
+                                class='px-4 py-3 rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 text-base w-100'
+
+                                />
+                                
+                                <button
+                                type="button"
+                                aria-label='Toggle password visibility'
+                                class='absolute inset-y-0 right-2 flex items-center text-gray-400 hover:text-gray-600'
+                                onClick={toggleVisibility}
+                                >Toggle</button>
+                            </div>
+
+                            <button class="mt-5 bg-sky-400 font-medium text-white rounded-md" type='submit'>Login</button>
+                            <h3 class='text-center'>
+                                Don't have an account?
+                                <A
+                                href={'/register'}
+                                class='font-medium text-sky-400 hover:text-sky-300'
+                                > Register</A>
+
+                            </h3>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        
+        </div>
+        </>
+    );
+}
+
+export default Register;

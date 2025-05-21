2bdc995d87fcaf7ebab342a66c55421f086b4237
Shawn <myking123321@gmail.com>
2025-05-21 19:34:44 +0700

    fix: switch route folder to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import { Router, Route } from "@solidjs/router";
 
-<<<<<<< HEAD
 import Login from "./routes/login";
 import page404 from "./routes/404";
-=======
-import Login from "./pages/login";
->>>>>>> 52aaa467541329133cb9fc8372e2da265e61667c
 
 function App() {
   return (
     <>
       <Router>
-<<<<<<< HEAD
           <Route path="/login" component={Login} />
           <Route path="*" component={page404}></Route>
-=======
-        <Route path="/login" component={Login} />
->>>>>>> 52aaa467541329133cb9fc8372e2da265e61667c
       </Router>
     </>
   );

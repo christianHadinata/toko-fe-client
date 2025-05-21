fac28b31383726f10124cc0f6a470008baf9d50e
Shawn <myking123321@gmail.com>
2025-05-21 14:44:56 +0700

    simpen bentar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Router, Route } from "@solidjs/router";
 
+import Login from "./routes/login";
+
 function App() {
   return (
     <>
+    <h1>A</h1>
+      <Router>
+          <Route path="/" component={Login} />
+      </Router>
     </>
   );
 }
---
+++ src/routes/login.jsx
@@ -0,0 +1,19 @@
+import { createEffect, createSignal } from 'solid-js'
+
+function Login(){
+    
+    return <>
+        <div class='name'>
+            <div class='flex h-screen w-1/2 flex-col items-center justify-center'>
+                <div class='w-1/2'>
+                    <A
+                    href='/'
+                    ></A>
+                    <h1>a</h1>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default Login

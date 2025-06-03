5ee4405daf54f3cda8e6ae1691003cda8ce27061
Christian Hadinata <christianhadinata2004@gmail.com>
2025-06-03 19:18:00 +0700

    fix: change toast timing to duration instread of unmountDelay

--- src/App.jsx
+++ src/App.jsx
@@ -14,91 +14,51 @@ import Cart from "./pages/cart";
 import ProfilePage from "./pages/profile";
 import Navbar from "./components/Navbar";
 
-
-
 function App() {
-  const toastOpts = {
-
-  }
-  createEffect(()=>{
+  const toastOpts = {};
+  createEffect(() => {
     const res = toastSignal();
-  
-    
-    if ( res && 'success' in res) {
+
+    if (res && "success" in res) {
       if (res.success) {
-        toast.success(res.message || "Default Message : Success" ,{
-          unmountDelay: 1000,
-      });
-      }else {
-        toast.error(res.message || "Default Message : Failed",{
-          unmountDelay: 1000,
+        toast.success(res.message || "Default Message : Success", {
+          duration: 3000,
+          // unmountDelay: 1000,
         });
-  
+      } else {
+        toast.error(res.message || "Default Message : Failed", {
+          duration: 3000,
+          // unmountDelay: 1000,
+        });
       }
     }
-  }); 
-  
+  });
+
   const MainLayout = (props) => {
     return (
       <>
         <div class="w-full flex justify-center p-4">
-          <div class="w-full max-w-7xl">
-            {props.children}</div>
+          <div class="w-full max-w-7xl">{props.children}</div>
         </div>
       </>
     );
   };
 
-
   return (
     <>
       <Router>
-        <Route
-          path="/login"
-          component={Login}
-        />
-        <Route
-          path="/register"
-          component={Register}
-        />
-        <Route
-          path="/"
-          component={Navbar}
-        >
-
-          <Route
-            path="/profile"
-            component={ProfilePage}
-          />
-          <Route
-            path="/"
-            component={MainLayout}
-          >
-            <Route
-              path="/product-details/:product_id"
-              component={Product}
-            />
-            <Route
-              path="/"
-              component={Home}
-            />
-            <Route
-              path="/category/:category_name"
-              component={Category}
-            />
-            <Route
-              path="/search"
-              component={Search}
-            />
-            <Route
-              path="/cart"
-              component={Cart}
-            />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/" component={Navbar}>
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/" component={MainLayout}>
+            <Route path="/product-details/:product_id" component={Product} />
+            <Route path="/" component={Home} />
+            <Route path="/category/:category_name" component={Category} />
+            <Route path="/search" component={Search} />
+            <Route path="/cart" component={Cart} />
           </Route>
-          <Route
-            path="*"
-            component={page404}
-          ></Route>
+          <Route path="*" component={page404}></Route>
         </Route>
       </Router>
     </>

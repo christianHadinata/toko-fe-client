6fd1b7923f6044b2b00d89e5f13e870c3e7b1837
Christian Hadinata <christianhadinata2004@gmail.com>
2025-06-05 17:48:56 +0700

    feat: add multiple signal technique, to prevent re render to all CartItem when updating just one CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import { createSignal, onMount, Show } from "solid-js";
 import { formatCurrency } from "../utils/formatCurrency";
 
 export default function CartItem(props) {
-  const [product, setProduct] = createSignal({});
+  const [productData, setProductData] = createSignal(null);
 
   onMount(() => {
     fetchSingleProductData();
@@ -11,98 +11,89 @@ export default function CartItem(props) {
   const fetchSingleProductData = async () => {
     try {
       const res = await fetch(
-        `http://localhost:5000/api/v1/products/${props.product.product_id}`
+        `http://localhost:5000/api/v1/products/${props.product().product_id}`
       );
       const data = await res.json();
 
-      const addedFormatCurrencyData = {
+      setProductData({
         ...data,
         formatted_product_price: formatCurrency(data.product_price),
-      };
-
-      console.log(addedFormatCurrencyData);
-      setProduct(addedFormatCurrencyData);
+      });
+      console.log(productData());
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div class="rounded-lg p-5 border-2 border-gray-400 md:grid md:grid-cols-[1fr_5fr] gap-x-10 w-full ">
-      <img
-        src={`http://localhost:5000/${product().product_featured_image_url}`}
-        alt=""
-        class="w-full max-h-40 object-contain self-center mb-4 md:mb-0"
-      />
-      <div class="flex flex-col gap-y-6 justify-between">
-        <div>
-          <h2 class="capitalize font-semibold text-clip text-xl">
-            {product().product_name}
-          </h2>
-          <p class="text-clip-2 text-gray-500 ">{product().product_details}</p>
-        </div>
-        <div>
-          <p class="">Stock: {product().product_stock}</p>
-          <div class="flex justify-between items-end flex-1">
-            <p class="font-bold text-sky-400 md:text-lg">
-              Price: {product().formatted_product_price}
+    <Show when={productData()}>
+      <div class="rounded-lg p-5 border-2 border-gray-400 md:grid md:grid-cols-[1fr_5fr] gap-x-10 w-full ">
+        <img
+          src={`http://localhost:5000/${
+            productData().product_featured_image_url
+          }`}
+          alt=""
+          class="w-full max-h-40 object-contain self-center mb-4 md:mb-0"
+        />
+        <div class="flex flex-col gap-y-6 justify-between">
+          <div>
+            <h2 class="capitalize font-semibold text-clip text-xl">
+              {productData().product_name}
+            </h2>
+            <p class="text-clip-2 text-gray-500 ">
+              {productData().product_details}
             </p>
+          </div>
+          <div>
+            <p class="">Stock: {productData().product_stock}</p>
+            <div class="flex justify-between items-end flex-1">
+              <p class="font-bold text-sky-400 md:text-lg">
+                Price: {productData().formatted_product_price}
+              </p>
 
-            <div class=" flex items-center">
-              <button
-                class="cursor-pointer rounded-md p-2 hover:bg-gray-100 focus:bg-gray-200"
-                onclick={props.onDelete}
-              >
-                <img src="/assets/icon/deleteBin.png" alt="" class="w-8" />
-              </button>
-              <div class="flex items-center justify-center rounded-2xl">
+              <div class=" flex items-center">
                 <button
-                  onclick={props.onDecreaseQuantity}
-                  class={`px-4 py-2 text-lg rounded-md 
+                  class="cursor-pointer rounded-md p-2 hover:bg-gray-100 focus:bg-gray-200"
+                  onclick={props.onDelete}
+                >
+                  <img src="/assets/icon/deleteBin.png" alt="" class="w-8" />
+                </button>
+                <div class="flex items-center justify-center rounded-2xl">
+                  <button
+                    onclick={props.onDecreaseQuantity}
+                    class={`px-4 py-2 text-lg rounded-md 
                     ${
-                      props.product.product_quantity === 1
+                      props.product().product_quantity === 1
                         ? "cursor-not-allowed text-gray-500"
                         : "cursor-pointer text-black hover:bg-gray-100"
                     }`}
-                  disabled={props.product.product_quantity === 1}
-                >
-                  -
-                </button>
-                <p class="text-lg p-4">{props.product.product_quantity}</p>
-                <button
-                  onclick={props.onIncreaseQuantity}
-                  class={`px-4 py-2  text-lg rounded-md 
+                    disabled={props.product().product_quantity === 1}
+                  >
+                    -
+                  </button>
+                  <p class="text-lg p-4">{props.product().product_quantity}</p>
+                  <button
+                    onclick={props.onIncreaseQuantity}
+                    class={`px-4 py-2  text-lg rounded-md 
                     ${
-                      props.product.product_quantity ===
-                      props.product.product_stock
+                      props.product().product_quantity ===
+                      productData().product_stock
                         ? "cursor-not-allowed text-gray-500"
                         : "cursor-pointer text-black hover:bg-gray-100"
                     }`}
-                  disabled={
-                    props.product.product_quantity ===
-                    props.product.product_stock
-                  }
-                >
-                  +
-                </button>
+                    disabled={
+                      props.product().product_quantity ===
+                      productData().product_stock
+                    }
+                  >
+                    +
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      {/* <div class="flex w-full items-end bg-red-200">
-        <button class="cursor-pointer">
-          <img
-            src="/assets/icon/deleteBin.png"
-            alt=""
-            class="max-w-7 max-h-7"
-          />
-        </button>
-        <button>-</button>
-        <input type="text" />
-        <button>+</button>
-      </div> */}
-    </div>
+    </Show>
   );
 }
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,13 +1,22 @@
-import { createEffect, createSignal, For, onMount } from "solid-js";
+import {
+  createEffect,
+  createSignal,
+  For,
+  onMount,
+  Switch,
+  Match,
+} from "solid-js";
 import CartItem from "../components/CartItem";
 import { setToastSignal } from "../stores/toaster";
 import { Toaster } from "solid-toast";
 import Modal from "../components/Modal";
+import { useNavigate } from "@solidjs/router";
 
 export default function Cart() {
   const [cartItems, setCartItems] = createSignal([]);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = createSignal(false);
-  const [deletingCartItemData, setDeletingCartItemData] = createSignal({});
+  const [deletingCartItemSignal, setDeletingCartItemSignal] =
+    createSignal(null);
 
   onMount(() => {
     fetchUserCartData();
@@ -28,7 +37,8 @@ export default function Cart() {
 
       console.log(data);
 
-      setCartItems(data);
+      const itemSignals = data.map((item) => createSignal(item));
+      setCartItems(itemSignals);
     } catch (error) {
       console.error(error);
     }
@@ -36,12 +46,11 @@ export default function Cart() {
 
   const handleDelete = async () => {
     try {
+      const cartItem = deletingCartItemSignal()?.[0]();
       const token = localStorage.getItem("token");
 
       const res = await fetch(
-        `http://localhost:5000/api/v1/cart/${
-          deletingCartItemData().cart_item_id
-        }`,
+        `http://localhost:5000/api/v1/cart/${cartItem.cart_item_id}`,
         {
           method: "DELETE",
           headers: {
@@ -53,19 +62,15 @@ export default function Cart() {
 
       let data = await res.json();
 
-      console.log(data);
-
       if (data.success) {
         data = {
           ...data,
-          message: `${
-            deletingCartItemData().product_name
-          } Deleted Succesfully!`,
+          message: `${cartItem.product_name} Deleted Successfully!`,
         };
+
         setCartItems(
           cartItems().filter(
-            (cartItem) =>
-              cartItem.cart_item_id != deletingCartItemData().cart_item_id
+            ([item]) => item().cart_item_id !== cartItem.cart_item_id
           )
         );
       }
@@ -78,30 +83,18 @@ export default function Cart() {
     }
   };
 
-  // const onDecreaseQuantity = (productId) => {
-  //   const idxProduct = products().findIndex(
-  //     (product) => product.product_id === productId
-  //   );
-  //   const updatedProduct = { ...products()[idxProduct] };
-  //   updatedProduct.product_quantity -= 1;
+  const updateQuantity = async (cartItemSignal, change) => {
+    const [cartItem, setCartItem] = cartItemSignal;
+    const newQty = cartItem().product_quantity + change;
+    if (newQty <= 0) return;
 
-  //   console.log(updatedProduct);
-  //   const newProducts = [...products()];
-
-  //   newProducts[idxProduct] = updatedProduct;
-
-  //   setProducts(newProducts);
-  // };
-  const handleDecreaseQuantity = async (cartItem) => {
     try {
       const token = localStorage.getItem("token");
 
-      const body = {
-        product_quantity: cartItem.product_quantity - 1,
-      };
+      const body = { product_quantity: newQty };
 
       const res = await fetch(
-        `http://localhost:5000/api/v1/cart/${cartItem.cart_item_id}`,
+        `http://localhost:5000/api/v1/cart/${cartItem().cart_item_id}`,
         {
           method: "PATCH",
           headers: {
@@ -112,103 +105,114 @@ export default function Cart() {
         }
       );
 
-      let data = await res.json();
+      const data = await res.json();
 
       if (data.success) {
-        const updatedCartItems = cartItems().map((item) =>
-          item.cart_item_id === cartItem.cart_item_id
-            ? { ...item, product_quantity: item.product_quantity - 1 }
-            : item
-        );
-        setCartItems(updatedCartItems);
+        setCartItem({ ...cartItem(), product_quantity: newQty });
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleIncreaseQuantity = async (cartItem) => {
-    try {
-      const token = localStorage.getItem("token");
+  const navigate = useNavigate();
 
-      const body = {
-        product_quantity: cartItem.product_quantity + 1,
-      };
-
-      const res = await fetch(
-        `http://localhost:5000/api/v1/cart/${cartItem.cart_item_id}`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body),
-        }
-      );
-
-      let data = await res.json();
-
-      if (data.success) {
-        const updatedCartItems = cartItems().map((item) =>
-          item.cart_item_id === cartItem.cart_item_id
-            ? { ...item, product_quantity: item.product_quantity + 1 }
-            : item
-        );
-        setCartItems(updatedCartItems);
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  const handleNavigate = () => {
+    navigate("/");
   };
+
+  // This code is create, to handle problem:
+  // when we had products in cart, then we delete one by one
+  // when the last product was deleted, the page will show empty cart page that can't be scrolled
+  // after that state, for example when we navigate to home page, the page can't be scrolled too
+  // so this code is used to clear overflow outcome from using Modal components, to make sure page is scrollable again
+  createEffect(() => {
+    if (cartItems().length === 0) {
+      document.body.style.overflow = ""; // pastikan scroll aktif
+    }
+  });
 
   return (
     <>
       <div class="flex flex-col gap-8 items-center">
-        <h1 class="font-semibold text-3xl">Cart Items</h1>
         <Toaster position="top-center" gutter={24} />
-        <For each={cartItems()}>
-          {(cartItem, index) => (
-            <CartItem
-              product={cartItem}
-              onDelete={() => {
-                setDeletingCartItemData(cartItem);
-                setIsDeleteModalOpen(true);
-              }}
-              onDecreaseQuantity={() => handleDecreaseQuantity(cartItem)}
-              onIncreaseQuantity={() => handleIncreaseQuantity(cartItem)}
-            />
-          )}
-        </For>
-        <Modal
-          open={isDeleteModalOpen()}
-          onClose={() => setIsDeleteModalOpen(false)}
-        >
-          <div class="p-6 text-center">
-            <h2 class="text-xl lg:text-2xl font-bold mb-4 py-2">Warning !</h2>
-            <p class="mb-4 text-lg lg:text-xl py-2">
-              Are you sure about deleting{" "}
-              <span class="font-bold">
-                {deletingCartItemData().product_name}
-              </span>{" "}
-              ?
-            </p>
-            <div class="flex justify-center gap-x-12 py-2">
-              <button
-                onClick={() => setIsDeleteModalOpen(false)}
-                class="px-4 py-2 bg-red-400 text-white rounded-lg text-base lg:text-lg cursor-pointer "
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleDelete}
-                class="px-4 py-2 bg-sky-400 text-white rounded-lg text-base lg:text-lg cursor-pointer"
-              >
-                Delete
-              </button>
+        <Switch>
+          <Match when={cartItems().length === 0}>
+            <div class="w-full min-h-screen pb-44 flex items-center justify-center">
+              <div class="flex flex-col items-center text-center px-4">
+                <img
+                  src="/assets/Image/empty-cart.png"
+                  alt=""
+                  class="w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-[28rem] lg:h-[28rem] object-contain"
+                />
+                <p class="font-semibold text-xl sm:text-2xl capitalize mt-2">
+                  Your cart is currently empty!
+                </p>
+                <p class="text-base sm:text-lg text-gray-500 py-4 max-w-md">
+                  Looks like you haven't added anything to your cart yet
+                </p>
+                <button
+                  onClick={handleNavigate}
+                  class=" mt-5 bg-sky-400 px-6 py-3 text-base sm:text-lg font-medium capitalize text-white rounded-lg hover:cursor-pointer hover:scale-105 transition-all"
+                >
+                  Go To Home
+                </button>
+              </div>
             </div>
-          </div>
-        </Modal>
+          </Match>
+          <Match when={cartItems().length > 0}>
+            <h1 class="font-semibold text-3xl">Cart Items</h1>
+            <For each={cartItems()}>
+              {(cartItemSignal) => {
+                const [cartItem, setCartItem] = cartItemSignal;
+                return (
+                  <CartItem
+                    product={cartItem}
+                    onDelete={() => {
+                      setDeletingCartItemSignal(cartItemSignal);
+                      setIsDeleteModalOpen(true);
+                    }}
+                    onDecreaseQuantity={() =>
+                      updateQuantity(cartItemSignal, -1)
+                    }
+                    onIncreaseQuantity={() => updateQuantity(cartItemSignal, 1)}
+                  />
+                );
+              }}
+            </For>
+            <Modal
+              open={isDeleteModalOpen()}
+              onClose={() => setIsDeleteModalOpen(false)}
+            >
+              <div class="p-6 text-center">
+                <h2 class="text-xl lg:text-2xl font-bold mb-4 py-2">
+                  Warning !
+                </h2>
+                <p class="mb-4 text-lg lg:text-xl py-2">
+                  Are you sure about deleting{" "}
+                  <span class="font-bold">
+                    {deletingCartItemSignal()?.[0]().product_name}
+                  </span>{" "}
+                  ?
+                </p>
+                <div class="flex justify-center gap-x-12 py-2">
+                  <button
+                    onClick={() => setIsDeleteModalOpen(false)}
+                    class="px-4 py-2 bg-red-400 text-white rounded-lg text-base lg:text-lg cursor-pointer "
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleDelete}
+                    class="px-4 py-2 bg-sky-400 text-white rounded-lg text-base lg:text-lg cursor-pointer"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          </Match>
+        </Switch>
       </div>
     </>
   );
